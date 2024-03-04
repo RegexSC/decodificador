1c168a3fe431f87d5c1c3f4157ce433ee10687e6
RegexSC <reginaldocamargo813@gmail.com>
2024-03-03 23:14:10 -0300

    UpGrade de layout

--- js/script.js
+++ js/script.js
@@ -3,21 +3,20 @@ let decrypt = document.querySelector(".decrypt-text");
 const matrizletters =[["e","enter"], ["i", "imes"],["a","ai"], ["u","uat"],["b","axei"], ["c","top"], ];
 
 function btEncryptext(){
- let decryption = encryptext(encrypt.value);
- decrypt.value =  decryption;
+ let encryption = encryptext(encrypt.value);
+ decrypt.value =  encryption;
  encrypt.value="";
 
 }
 
 function btDecryptext(){
-    let encryption = decryptext(decrypt.value);
-    encrypt.value =  encryption;
-    decrypt.value="";
+    let  decryption= decryptext(encrypt.value);
+    decrypt.value =  decryption;
+    encrypt.value="";
    
    }
 
 function encryptext(encryptedText){
-    // const matrizletters =[["e","enter"], ["i", "imes"], ["a","ai"], ["u","uat"], ["o","ober"]];
     encryptedText = encryptedText.toLowerCase();
     
     for(i = 0; i < matrizletters.length; i++){
@@ -25,12 +24,11 @@ function encryptext(encryptedText){
             encryptedText=encryptedText.replaceAll(matrizletters[i][0],matrizletters[i][1]);
         }
     }
-    console.log(encryptedText)
+    
     return encryptedText;
 }
 
 function decryptext(decryptedText){
-    // const matrizletters =[["e","enter"], ["i", "imes"],["a","ai"],["o","ober"], ["u","uat"], ["c","top"]];
     decryptedText = decryptedText.toLowerCase();
 
     for(i = 0; i < matrizletters.length; i++){
